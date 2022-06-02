2178049e1e21f2f00078f600e49736fbf7926414
intervinn <nikita.bondarenko335@gmail.com>
2022-06-03 00:38:32 +0300

    k

---
+++ config.py
@@ -0,0 +1,9 @@
+botconfig = {
+    "channelId": 949759833404506185, # Id of channel where your message is
+    "messageId": 982016578310799481, # Id of your message to put reactions below
+    "reactionRoles": { 
+        "🤨": 982016613236760657, # emoji of reaction and Id of role player will get for reacting to that emoji
+    }
+
+    # you can add even more reaction roles
+} 
